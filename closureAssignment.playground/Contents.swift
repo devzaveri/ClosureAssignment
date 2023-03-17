93ca098ffad3c4982c89ce44dbcd09cd9dbadbb3
Dev Zaveri <84063889+devzaveri@users.noreply.github.com>
2023-03-17 11:50:25 +0530

    Update Contents.swift

--- closureAssignment.playground/Contents.swift
+++ closureAssignment.playground/Contents.swift
@@ -1,16 +1,15 @@
 import Foundation
 import UIKit
 
-// Decelaring Array arr1 and arr2
+
 var arr1: [String] = ["b", "a", "d", "e", "c"]
 var arr2: [Int] = [4, 3, 2, 5, 1]
 
 
-//Sort that arrays and add that into arrA and arrB
 let arrA: () = arr1.sort()
 let arrB: () = arr2.sort(by: >)
 
-//decelaring closure
+
 
 let dataTaskPerfome = { (arrA: [String] , arrB: [Int]) -> [Any] in
     var mergedArray = arr1.flatMap { [$0] } + arr2.map { Int($0) }
@@ -24,6 +23,6 @@ func values(arr:()->()) {
     arr()
 }
 
-//calling that closure
+
 
 values {}
